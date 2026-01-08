f953a53f78162f99b8e0b5e459f08a34a6956ce6
Laraib207 <laraibsiddiqui10ki@gmail.com>
2026-01-08 16:51:08 +0530

    SSt

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,31 @@
+// import Navbar from "@/app/components/Navbar";
+// import Hero from "@/app/components/Hero";
+// import Services from "@/app/components/Services";
+// import Footer from "@/app/components/Footer";
+
+// export default function Home() {
+//   console.log("Rendering Home Page");
+//   return (
+//     <>
+//       <Navbar />
+//       <Hero />
+//       <Services />
+//       <Footer />
+//     </>
+//   );
+// }
+
+
+
 import Navbar from "@/app/components/Navbar";
 import Hero from "@/app/components/Hero";
 import Services from "@/app/components/Services";
 import Footer from "@/app/components/Footer";
 
 export default function Home() {
+  console.log("Rendering Home Page");
+  console.log("API URL:", process.env.NEXT_PUBLIC_API_URL);
+
   return (
     <>
       <Navbar />
